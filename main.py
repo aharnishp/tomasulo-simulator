8337c40296a39ac6f0362caf0b88ce4cf21033f2
Aharnish <aharnishpithva@gmail.com>
2023-02-27 14:51:28 +0530

    restructured

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
+# This is a python script to simulate execution of instructions in CPU using Tomasulo's Algorithm
+# 
+# Usage: Modify the "instQueue" variable for executing custom instructions, the format is provided
+#
+# WIP: To advance press enter to skip to next clock cycle.
+# 
+# 
+
+
+# Notes: Broadcast and Write Back are used as different terms here:
+#    -> Broadcast refers to the results availibility to other instructions
+#               Hence simulating forwarding inside a CPU
+#
+#    -> WriteBack refers to the part when result is stored in the memory.
+#          
+#    -> In this simulator Write Back occurs after 1 cycle of Broadcasting.
+
+
+
+
+
+
 import numpy as np
 
+##### INPUT VARIABLES #####
 
-# Simlulation Parameters
 
 # Instruction Queue
 ## Format [[indx, Operation, storeTo, input1, input2, queuedStatus],]
@@ -16,123 +38,219 @@ instQueue = {
     2:["sub", 4, 1, 2, 0],
     3:["add", 1, 2, 3, 0],
 }
-# instQueue = {
-#     0:["add", 2, 4, 1, 0],
-#     1:["div", 1, 2, 1, 0],
-#     2:["sub", 4, 1, 2, 0],
-#     3:["add", 1, 2, 3, 0],
-# }
+
 
 # Architecture of core distribution
-## Format: [[["list of ","operations supported"], #number of rows in Reservation Table]]
-cores = [[["add", "adi", "sub", "subi", "and", "or", "xor", "not"],3], [["mul", "div"],3], [["mul", "div"],3]]
-
-# Register File
-## Format [data,] starting from 0
-regF = [2.72, 3.14, -0.27, 5.0]
-
-
-# R.A.Table
-## Format: [None, None, 3, None]
-rat = [None]
+## Format: [[["list of ","operations supported"], #number of rows in Reservation Table, [executionDuration]]]
+cores = [
+            [["add", "adi", "sub", "subi", "and", "or", "xor", "not"],3,[2,2,2,2,2,2,2,2]], ## coreID 0
+            [["mul", "div"],3,[10,40]],     ## coreID = 1
+            [["mul", "div"],3,[10,40]]      ## coreID = 2
+        ]
 
 
-# GLOBAL STATE VARIABLES
+## Dictionary of register file
+regF = {
+    0:2.72,
+    1:3.14,
+    2:-0.27,
+    3:5.0,
+}
+
+##### PARAMETERS #####
+writeback_delay = 1
 
 
-# Telemetry Settings
-dependencyTelemetry = 1
 
 
 
-# Main Memory
-mainMemory = []
 
-def storeToReg(addr, value):
-    # increase length of list if list is short
-    if(len(regF) < addr):
-        for _ in range(addr - len(regF)+1):
-            regF.append(0)
-    regF[addr] = value
+### Telemetries
+dispatchTelemetry = 1
+broadcastTelemetry = 1
 
-def getReg(addr):
-    # increase length of list if list is short
-    if(len(regF) < addr):
-        for _ in range(addr - len(regF) + 1):
-            regF.append(0)
-    return(regF[addr])
-
-def lookUpRAT(addr):
-    if(len(rat) < addr):
-        for _ in range(addr - len(rat) + 1):
-            rat.append(None)
-    if(type(rat[addr])==None):
-        return -1
-    else:
-        return rat[addr]
+##### OPERATIONAL VARIABLES #####
 
 
-def dependencyFinder(instQue):
-    # Table of which instruction generates value for register
-    ## Format: registerIndex: LastInstructionStoredIndex
-    rut = {}
+## Creating reservation table
+# Format = {
+#   coreID: [        Vj = float         Qj = pointer stored as string
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageClockCycle],
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageClockCycle],
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageClockCycle],
+#   ],
+#   coreID: [
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageClockCycle],
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageClockCycle],
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageClockCycle],
+#   ]
+# }
+
+# Qj examples
+# Qj = r12 pointing to regF[12]
+# Qj = c2i4 pointing to instruction stored in reservation table of Cth core and Ith instruction
+# Qj = i7 pointing to output of instructionId == 7
+
+
+# generate empty reservation tables
+resst={}
+coreCounter = 0
+for core in cores:
+    resst[coreCounter] = []
+    coreCounter += 1
+
+
+
+# queue to wait for the simulated execution to complete
+# Format = {
+#   clock1: [[insID, outputValue],[insID, outputValue]],
+#   clock2: [[insID, outputValue]],
+# }
+broadcastQueue = {}
+# similar use/format to above but occurs 1 clock late for each.
+writeBackQueue = {}
+
+
+# Format2 = [
+#   [clock1,[[insID, outputValue],[insID, outputValue]]],
+#   [clock2, [[insID, outputValue]]],
+# ]
+# broadcastQueue = []
+
+
+##### INITIALIZATIONS #####
+##### INITIALIZATIONS #####
+##### INITIALIZATIONS #####
+
+print("Empty reservation station:", resst)
+
+
+print("adding dummy data to reservation table")
+resst[0].append([2, 1, "add", 56, 34, None, None, None])
+
+
+##### FUNCTIONS #####
+##### FUNCTIONS #####
+##### FUNCTIONS #####
+
+
+def exec(opc, Val1, Val2):
+    if(opc == "add"):
+        return Val1 + Val2
+    elif(opc == "adi"):
+        return Val1 + Val2
+    elif(opc == "sub"):
+        return Val1 - Val2
+    elif(opc == "subi"):
+        return Val1 + Val2
+    elif(opc == "and"):
+        return Val1 & Val2
+    elif(opc == "or"):
+        return Val1 | Val2
+    elif(opc == "xor"):
+        return (Val1 ^ Val2)
+    elif(opc == "not"):
+        # val2 can be None
+        return not(Val1)
+    elif(opc == "mul"):
+        return Val1 * Val2
+    elif(opc == "div"):
+        return Val1 / Val2
+
+def evaluatePointer(inPointer):
+    if(inPointer[0]== 'r'):
+        return regF[inPointer[1:]]  ## return value from register file after removing 'r'
+    elif(inPointer[0]=="i"):
+        print("Un-updated pointer value was dispatched for execution.")
+        return None
+
+def broadcast(insID, outputValue):
+    RegtoStore = (instQueue[insID][2])
+    regF[RegtoStore] = outputValue
+    # print(insID, outputValue)
     
-    # List of all RAW dependencies in the instruction queue
-    ## Format: [[RegisterNumber, InstructionWritingToRegister, registerUsingInstruction, InputNumberOfReader]]
-    raw = []
-
-    # List of register values that were assigned just as an intermediate value
-    # Stale value?
-    ## Format: 
-    intermediaries = []
-
-    # List of all WAW dependencies in the instruction queue
-    ## Format = [[RegisterNumber, 1st inst. writing, 2nd inst. writing]]
-    waw = []
-
-    # listing all outputs that are inputs to next instructions
-    for indx in instQue:
-        # check if this instruction has dependency on previous
-        in1 = instQue[indx][2]
-        in2 = instQue[indx][3]
-        # input 1 of current inst depends on previous
-        if(in1 in rut.keys()):
-            raw.append([in1, rut[in1], indx, 0])
-        if(in2 in rut.keys()):
-            raw.append([in2, rut[in2], indx, 1])
-            
-        # checking before assigning to test WAW
-        # if(rut[(instQue[indx][1])] == indx):
-        #     pass
-        # assigning register with value of instruction index
-        rut[(instQue[indx][1])] = indx
-
-    return [rut, raw, intermediaries]
+    # replace existing pointers in reservation table to execute.
+    for coreId in resst:
+        for ins in resst[coreID]
 
 
-def issueInstructions():
-    pc = 0  ## program counter for pointing to Instruction Queue
+def dispatch(coreID, insNum):
+    ins = resst[coreID][insNum]
+    insId = ins[0]
+    insOp = ins[2]
+    
+    Vj = ins[3]
+    Vk = ins[4]
 
-    output = (dependencyFinder(instQue=instQueue))
-    if(dependencyTelemetry): print("Register Assignment Last", output[0])
-    if(dependencyTelemetry): 
-        for rawd in output[1]:
-            print("RAW Dependency:")
-            print("  Register #" ,rawd[0])
-            print("  depends on instr.", rawd[1])
-            print("  required by inst.", rawd[2], "for input", rawd[3])
+    Qj = ins[5]
+    Qk = ins[6]
+
+    ## operate according to OpCode
+    ### checking if core supports that operation
+    if(not(insOp in cores[coreID][0])):
+        print("Operation not supported by core.")
+        return -1
+    
+    # getting appropriate inputs for operation.
+    if(type(Vj) != type(None)):
+        Val1 = Vj
+    elif(type(Qj) != type(None)):
+        Val1 = evaluatePointer(Qj)
+    else:
+        Val1 = None
+
+    if(type(Vk) != type(None)):
+        Val2 = Vk
+    elif(type(Qk) != type(None)):
+        Val2 = evaluatePointer(Qk)
+    else:
+        Val2 = None
 
 
-def main():
-    # 
-    instLft = instQueue.copy()
-
-    while(1):
-        # Start issuing
-        issueInstructions()\
-
-        break
+    # execution of instruction begins (as a simulator, the value is processed and waits for clock)
+    outVal = exec(insOp, Val1, Val2)
 
 
-if(__name__ == "__main__"):
-    main()
+    # find delay from the core properties
+    delay = cores[coreID][2][(cores[coreID][0].index(insOp))]
+    targetClock = clock + delay
+
+    if(targetClock in broadcastQueue.keys()):
+        broadcastQueue[targetClock].append([insId, outVal])
+    else:
+        broadcastQueue[targetClock] = [[insId, outVal]]
+    
+    ## adding to writeback queue
+    if((targetClock+writeback_delay) in writeBackQueue.key()):  # checking if key already exists
+        print("Multiple writebacks in", (targetClock + writeback_delay), "clock cycle.")
+        writeBackQueue[targetClock+writeback_delay].append([insId, outVal])
+    else:
+        broadcastQueue[targetClock+writeback_delay] = [[insId, outVal]]
+
+clock = 0
+
+
+while(True):
+    print("clock cycle=", clock)
+    ## dispatch stage
+    for core in resst:
+        pass
+        # for ins in resst[core]:
+        #     print("pending core:", core, " > ",  ins)
+
+
+    # Broadcasting
+    if(clock in broadcastQueue.keys()):
+        pendingBrd = broadcastQueue[clock]
+
+        if(broadcastTelemetry): print("pendingbroadcasts: ", pendingBrd)
+        for brd in pendingBrd:
+            broadcast(insID=brd[0], outputValue=[1])
+        
+
+    # for brd in broadcastQueue:
+
+    # check for available broadcasts at this clock cycle
+
+    break
+    clock += 1
---
+++ main3.py
@@ -0,0 +1,160 @@
+import numpy as np
+
+
+# Simlulation Parameters
+
+# Instruction Queue
+## Format [[indx, Operation, storeTo, input1, input2, queuedStatus],]
+# instQueue = {
+#     0:["add", 2, 1, 0, 0],
+#     1:["mul", 3, 2, 1, 0],
+#     2:["sub", 5, 1, 0, 0],
+# }
+instQueue = {
+    0:["add", 2, 4, 1, 0],
+    1:["div", 1, 2, 1, 0],
+    2:["sub", 4, 1, 2, 0],
+    3:["add", 1, 2, 3, 0],
+}
+# instQueue = {
+#     0:["add", 2, 4, 1, 0],
+#     1:["div", 1, 2, 1, 0],
+#     2:["sub", 4, 1, 2, 0],
+#     3:["add", 1, 2, 3, 0],
+# }
+
+# Architecture of core distribution
+## Format: [[["list of ","operations supported"], #number of rows in Reservation Table]]
+cores = [[["add", "adi", "sub", "subi", "and", "or", "xor", "not"],3], [["mul", "div"],3], [["mul", "div"],3]]
+
+# Register File
+## Format [data,] starting from 0
+regF = [2.72, 3.14, -0.27, 5.0]
+
+
+# R.A.Table
+## Format: [None, None, 3, None]
+rat = [None]
+
+
+# GLOBAL STATE VARIABLES
+## Creating reservation table
+# Format = {
+#   coreID: [
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageNum],
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageNum],
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageNum],
+#   ],
+#   coreID: [
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageNum],
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageNum],
+#       [insID, Busy, OpCode, Vj, Vk, Qj, Qk, DispatchedStageNum],
+#   ]
+# }
+
+# generate empty reservation tables
+resst={}
+coreCounter = 0
+for core in cores:
+    resst[coreCounter] = []
+    coreCounter += 1
+
+print("Empty reservation statoin:", resst)
+
+
+# Telemetry Settings
+dependencyTelemetry = 1
+
+
+
+# Main Memory
+mainMemory = []
+
+def storeToReg(addr, value):
+    # increase length of list if list is short
+    if(len(regF) < addr):
+        for _ in range(addr - len(regF)+1):
+            regF.append(0)
+    regF[addr] = value
+
+def getReg(addr):
+    # increase length of list if list is short
+    if(len(regF) < addr):
+        for _ in range(addr - len(regF) + 1):
+            regF.append(0)
+    return(regF[addr])
+
+def lookUpRAT(addr):
+    if(len(rat) < addr):
+        for _ in range(addr - len(rat) + 1):
+            rat.append(None)
+    if(type(rat[addr])==None):
+        return -1
+    else:
+        return rat[addr]
+
+
+def dependencyFinder(instQue):
+    # Table of which instruction generates value for register
+    ## Format: registerIndex: LastInstructionStoredIndex
+    rut = {}
+    
+    # List of all RAW dependencies in the instruction queue
+    ## Format: [[RegisterNumber, InstructionWritingToRegister, registerUsingInstruction, InputNumberOfReader]]
+    raw = []
+
+    # List of register values that were assigned just as an intermediate value
+    # Stale value?
+    ## Format: 
+    intermediaries = []
+
+    # List of all WAW dependencies in the instruction queue
+    ## Format = [[RegisterNumber, 1st inst. writing, 2nd inst. writing]]
+    waw = []
+
+    # listing all outputs that are inputs to next instructions
+    for indx in instQue:
+        # check if this instruction has dependency on previous
+        in1 = instQue[indx][2]
+        in2 = instQue[indx][3]
+        # input 1 of current inst depends on previous
+        if(in1 in rut.keys()):
+            raw.append([in1, rut[in1], indx, 0])
+        if(in2 in rut.keys()):
+            raw.append([in2, rut[in2], indx, 1])
+            
+        # checking before assigning to test WAW
+        # if(rut[(instQue[indx][1])] == indx):
+        #     pass
+        # assigning register with value of instruction index
+        rut[(instQue[indx][1])] = indx
+
+    return [rut, raw, intermediaries]
+
+
+def issueInstructions():
+    pc = 0  ## program counter for pointing to Instruction Queue
+
+    output = (dependencyFinder(instQue=instQueue))
+    if(dependencyTelemetry): print("Register Assignment Last", output[0])
+    if(dependencyTelemetry): 
+        for rawd in output[1]:
+            print("RAW Dependency:")
+            print("  Register #" ,rawd[0])
+            print("  depends on instr.", rawd[1])
+            print("  required by inst.", rawd[2], "for input", rawd[3])
+
+
+def main():
+    # 
+    instLft = instQueue.copy()
+
+    while(1):
+        # Start issuing
+        issueInstructions()\
+
+        break
+
+
+if(__name__ == "__main__"):
+    main()
